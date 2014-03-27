2984f900786e12a236c454eea0209b79e788e1f2
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-03-27 15:46:43 -0400

    Actually the "Nothing is there to sync with Beeminder yet. Just code to work with logan, the Basecamp API wrapper gem." from first commit is in this commit.

--- code/basecamp.rb
+++ code/basecamp.rb
@@ -1,3 +1,9 @@
 require_relative 'base'
 
 require 'logan'
+
+basecamp_id = ENV[ 'BASECAMP_ID' ]
+auth_hash = { :username => ENV[ 'BASECAMP_USER' ], :password => ENV[ 'BASECAMP_PASS' ] }
+user_agent = ENV[ 'BASECAMP_HTTP_USERAGENT' ]
+
+@logan = Logan::Client.new( basecamp_id, auth_hash, user_agent )
